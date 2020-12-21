f3de98399a89e4b2c8922056014637d7b508ea74
Tuukka <tuukka.tapani.r@gmail.com>
2020-12-21 19:26:52 +0200

    change to dark theme

--- scripts/programs.sh
+++ scripts/programs.sh
@@ -2,7 +2,7 @@
 #
 # Import the colors
 
-chosen=$(echo -e "Cancel\nMusic\nGimp\nFiles\nEmacs\nPower" |dmenu -fn "terminus-medium-r-*-*-*-*" -nb "#fbf1c7" -nf "#3c3836" -sb "#cc241d" -sf "#e4ede4")
+chosen=$(echo -e "Cancel\nMusic\nGimp\nFiles\nEmacs\nPower" |dmenu -fn "terminus-medium-r-*-*-*-*" -nb "#16161e" -nf "#787c99" -sb "#f7768e" -sf "#acb0d0")
 
 if [[ $chosen = "Music" ]]; then
     urxvt -e musikcube
